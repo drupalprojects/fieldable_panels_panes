cea8f1d11018a2c7576c9caff5e300f3ae7eccf6
Damien McKenna <damien@mc-kenna.com>
2016-03-01 16:55:55 -0500

    By DamienMcKenna: Small comment improvements.

--- includes/PanelsPaneController.class.php
+++ includes/PanelsPaneController.class.php
@@ -129,21 +129,25 @@ class PanelsPaneController extends DrupalDefaultEntityController {
     }
 
     try {
+      // Since we already have an fpid, write the revision to ensure the vid is
+      // the most up to date, then write the record.
       if (!$entity->is_new) {
-        // Since we already have an fpid, write the revision to ensure the
-        // vid is the most up to date, then write the record.
         $this->saveRevision($entity);
         drupal_write_record('fieldable_panels_panes', $entity, 'fpid');
 
         field_attach_update('fieldable_panels_pane', $entity);
-        module_invoke_all('fieldable_panels_pane_update', $entity);
-        module_invoke_all('entity_update', $entity, 'fieldable_panels_pane');
 
+        // Trigger hook_fieldable_panels_pane_update().
+        module_invoke_all('fieldable_panels_pane_update', $entity);
+
+        // Trigger hook_entity_update().
+        module_invoke_all('entity_update', $entity, 'fieldable_panels_pane');
       }
+
+      // If this is new, write the record first so there's an fpid, then save
+      // the revision so that there's a vid. This means that the vid has to be
+      // written out again.
       else {
-        // If this is new, write the record first so we have an fpid,
-        // then save the revision so that we have a vid. This means we
-        // then have to write the vid again.
         drupal_write_record('fieldable_panels_panes', $entity);
         $this->saveRevision($entity);
         db_update('fieldable_panels_panes')
@@ -152,7 +156,11 @@ class PanelsPaneController extends DrupalDefaultEntityController {
           ->execute();
 
         field_attach_insert('fieldable_panels_pane', $entity);
+
+        // Trigger hook_fieldable_panels_pane_insert().
         module_invoke_all('fieldable_panels_pane_insert', $entity);
+
+        // Trigger hook_entity_insert().
         module_invoke_all('entity_insert', $entity, 'fieldable_panels_pane');
       }
 
